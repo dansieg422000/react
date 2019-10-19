159e8b2a4069aa4489ca35f3f62f360802f9a37d
Ronald <ronald_olay@yahoo.com>
2019-10-19 18:25:56 +1100

    Destructure and Map function added

--- src/components/Performance.js
+++ src/components/Performance.js
@@ -2,30 +2,17 @@ import React, { Component } from 'react';
 import {Grid, Table} from "@material-ui/core";
 import Container from "reactstrap/es/Container";
 import UserPerformanceReducers from "../reducers/userPerformanceReducers";
+import userPerformances from '../state/userPerformance';
 
 const performance = () => {
     return (
         <Container>
-            <Table>
-                <thead>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
-                </tr>
-                </thead>
-                <tbody>
-                    <UserPerformanceReducers/>
+            <UserPerformanceReducers users={userPerformances} />
 
-                </tbody>
-            </Table>
 
         </Container>
 
     );
-
-
-
 }
 
 export default (performance);
--- src/reducers/userPerformanceReducers.js
+++ src/reducers/userPerformanceReducers.js
@@ -1,14 +1,39 @@
 
-import userPerformance from '../state/userPerformance';
+import {Table} from "@material-ui/core";
+import React from "react";
 
-const userPerformanceList = ()  => {
-    console.log(userPerformance);
+const UserPerformanceList = ({users})  => {
+    console.log(users);
+
+    // return '';
 
     return (
-        ''
+        <Table>
+
+            <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                users.map(user =>
+                    <tr>
+                        <td>{user.firstName}</td>
+                        <td>{user.lastName}</td>
+                        <td>{user.email}</td>
+                    </tr>
+
+            )}
+
+
+            </tbody>
+        </Table>
     )
 }
 
-export default userPerformanceList;
+export default UserPerformanceList;
 
 
